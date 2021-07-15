23fc403c061aaae765575276479c722652d71d58
umerkang66 <umerkang66@gmail.com>
2021-07-15 09:31:59 +0500

    initial commit of jbook application, and added two plugins in the esbuild transpiler

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,57 @@
-import React from 'react';
+import * as esbuild from 'esbuild-wasm';
+import { ChangeEvent, FC, useEffect, useRef } from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App: FC = (): JSX.Element => {
+  const ref = useRef<any>();
+  const [input, setinput] = useState('');
+  const [code, setCode] = useState('');
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClickHandler = async () => {
+    if (!ref.current) return;
+
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    });
+
+    setCode(result.outputFiles[0].text);
+  };
+
+  const onChangeHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setinput(e.target.value);
+  };
+
+  return (
+    <div className="app">
+      <textarea value={input} onChange={onChangeHandler}></textarea>
+      <div>
+        <button onClick={onClickHandler}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
