ef7e61fd002f19b06f1b08576358824de46172ba
Umer Kang <umerkang66@gmail.com>
2022-03-25 22:08:03 +0500

    add new version of jbook frontend application

---
+++ README.md
@@ -0,0 +1,3 @@
+# JBook Application
+
+Write JavaScript Code and and see the result preview, and Write notes about your code snippet
---
+++ src/components/cell-list.tsx
@@ -0,0 +1,31 @@
+import '../styles/cell-list.css';
+import { FC, Fragment, useEffect } from 'react';
+// Actions
+import { useTypedSelector } from '../hooks/use-typed-selector';
+
+// Components
+import CellListItem from './cell-list-item';
+import AddCell from './add-cell';
+
+const CellList: FC = () => {
+    const cells = useTypedSelector(({ cells: { order, data } }) => {
+        // Get cells according the cellsState.order
+        return order.map(id => data[id]);
+    });
+
+    const renderedCells = cells.map(cell => (
+        <Fragment key={cell.id}>
+            <CellListItem cell={cell} />
+            <AddCell previousCellId={cell.id} />
+        </Fragment>
+    ));
+
+    return (
+        <div className="cell-list">
+            <AddCell forceVisible={cells.length === 0} previousCellId={null} />
+            {renderedCells}
+        </div>
+    );
+};
+
+export default CellList;
